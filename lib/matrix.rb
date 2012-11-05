fc2c57d1598e2de3f7d5cb33f65123a3580e1b11
Eduardo Segredo <edusegre@gmail.com>
2012-11-05 16:29:50 +0000

    * operator has been incorporated to the Matrix class. Some unit tests have been added

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,16 +1,16 @@
 require 'fraction'
 
 class Matrix
-  attr_reader :numberRows
-  attr_reader :numberColumns
+	attr_reader :numberRows
+	attr_reader :numberColumns
 
-  def initialize(*args)
-	  raise IndexError unless ((args[0] > 0) && (args[1] > 0))
-    @numberRows = args[0]
-    @numberColumns = args[1]
-    @data = Array.new(@numberRows, Array.new(@numberColumns, args[2])) if (args.size == 3)
-    @data = Array.new(@numberRows, Array.new(@numberColumns)) if (args.size == 2)
-  end
+	def initialize (*args)
+		raise IndexError unless ((args[0] > 0) && (args[1] > 0))
+		@numberRows = args[0]
+		@numberColumns = args[1]
+		@data = Array.new(@numberRows, Array.new(@numberColumns, args[2])) if (args.size == 3)
+		@data = Array.new(@numberRows, Array.new(@numberColumns)) if (args.size == 2)
+	end
 	
 	def [] (index)
 		@data[index]
@@ -32,6 +32,21 @@ class Matrix
 		for i in (0 ... @numberRows)
 			for j in (0 ... @numberColumns)
 				result[i][j] = self[i][j] + m[i][j]
+			end
+		end
+		return result
+	end
+
+	def * (m)
+		raise IndexError unless (@numberColumns == m.numberRows)
+		result = Matrix.new(@numberRows, m.numberColumns)
+		for i in (0 ... @numberRows)
+			for j in (0 ... m.numberColumns)
+				sum = self[i][0] * m[0][j]
+				for k in (1 ... @numberColumns)
+					sum += self[i][k] * m[k][j]
+				end
+				result[i][j] = sum
 			end
 		end
 		return result
--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -10,9 +10,11 @@ describe Matrix do
 		expect {Matrix.new(0, 1, 20)}.to raise_error(IndexError)
 		expect {Matrix.new(-1, 4)}.to raise_error(IndexError)
 		expect {Matrix.new(-1, 4, 17)}.to raise_error(IndexError)
+		expect {Matrix.new(1, 0, Fraction.new(0, 1))}.to raise_error(IndexError)
+		expect {Matrix.new(1, 1, Fraction.new(1, 0))}.to raise_error(TypeError)
 	end
 
-	it "Se debe poder sumar dos matrices de enteros" do
+	it "Se debe poder sumar dos matrices de Enteros" do
 		(Matrix.new(3, 2, 4) + Matrix.new(3, 2, 1)).should == Matrix.new(3, 2, 5)
 		(Matrix.new(4, 7, -3) + Matrix.new(4, 7, -4)).should == Matrix.new(4, 7, -7)
 		(Matrix.new(20, 43, -1) + Matrix.new(20, 43, 5)).should == Matrix.new(20, 43, 4)
@@ -21,8 +23,23 @@ describe Matrix do
 		expect {(Matrix.new(5, 8, -1) + Matrix.new(5, 8, 5)) == Matrix.new(8, 5, 4)}.to raise_error(IndexError)
 	end
 
-	it "Se debe poder sumar dos matrices de racionales" do
+	it "Se debe poder sumar dos matrices de Racionales" do
 		(Matrix.new(3, 2, Fraction.new(1, 2)) + Matrix.new(3, 2, Fraction.new(1, 6))).should == Matrix.new(3, 2, Fraction.new(8, 12))
+		(Matrix.new(4, 7, Fraction.new(-3, 4)) + Matrix.new(4, 7, Fraction.new(-4, 2))).should == Matrix.new(4, 7, Fraction.new(-22, 8))
+		(Matrix.new(20, 43, Fraction.new(-1, 2)) + Matrix.new(20, 43, Fraction.new(8, 4))).should == Matrix.new(20, 43, Fraction.new(12, 8))
+		(Matrix.new(12, 10, Fraction.new(1, 2)) + Matrix.new(12, 10, Fraction.new(1, 2))).should_not == Matrix.new(12, 10, Fraction.new(1, 3))
+		expect {Matrix.new(5, 8, Fraction.new(-1, 2)) + Matrix.new(8, 5, Fraction.new(5, 2))}.to raise_error(IndexError)
+		expect {(Matrix.new(5, 8, Fraction.new(-1, 2)) + Matrix.new(5, 8, Fraction.new(5, 2))) == Matrix.new(8, 5, Fraction.new(4, 7))}.to raise_error(IndexError)
+	end
+
+	it "Se debe poder multiplicar dos matrices de Enteros" do
+		(Matrix.new(3, 2, 2) * Matrix.new(2, 3, 3)).should == Matrix.new(3, 3, 12)
+		(Matrix.new(3, 2, 7) * Matrix.new(2, 4, 3)).should == Matrix.new(3, 4, 42)
+	end
+
+	it "Se debe poder multiplicar dos matrices de Racionales" do
+		(Matrix.new(3, 2, Fraction.new(1, 2)) * Matrix.new(2, 3, Fraction.new(3, 5))).should == Matrix.new(3, 3, Fraction.new(6, 10))
+		(Matrix.new(3, 2, Fraction.new(3, 5)) * Matrix.new(2, 4, Fraction.new(7, 6))).should == Matrix.new(3, 4, Fraction.new(42, 30))
 	end
 
 end
